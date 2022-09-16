a1003b45d1349405ef833cc18031922aac5080da
Luis Mayta <luis@hadenlabs.com>
2022-09-15 23:32:41 -0300

    chore: enabled handlebars with express (#3)

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express"); //
 const app = express();
 const morgan = require("morgan"); //
-const cors = require("cors");
-const router = require("./src/routes/index");
-const pool = require("./src/database/database.js");
+
+// const cors = require("cors");
+// const router = require("./src/routes/index");
+// const pool = require("./src/database/database.js");
+
+const { create } = require("express-handlebars");
+
+const hbs = create({
+  layoutsDir: __dirname + "/src/views/layouts",
+  extname: ".hbs",
+  defaultLayout: "index",
+  partialsDir: __dirname + "/src/views/partials/",
+});
+
+app.engine(".hbs", hbs.engine);
+
+app.set("view engine", ".hbs");
 
 app.set("port", 4000);
 
-//middlewares
 app.use(morgan("dev"));
 
 //routes
@@ -15,7 +28,39 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//iniciar
+fakeApi = () => {
+  return [
+    {
+      name: "Katarina",
+      lane: "midlaner",
+    },
+    {
+      name: "Jayce",
+      lane: "toplaner",
+    },
+    {
+      name: "Heimerdinger",
+      lane: "toplaner",
+    },
+    {
+      name: "Zed",
+      lane: "midlaner",
+    },
+    {
+      name: "Azir",
+      lane: "midlaner",
+    },
+  ];
+};
+
+app.get("/", (req, res) => {
+  res.render("main", {
+    layout: "index",
+    suggestedChamps: fakeApi(),
+    listExists: true,
+  });
+});
+
 app.listen(app.get("port"), () => {
   console.log("Listening on port ", app.get("port"));
 });
